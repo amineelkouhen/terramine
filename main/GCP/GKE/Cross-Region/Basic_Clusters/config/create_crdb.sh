a361f7c7fc4eec81c8aea1626dfbdf6839d188c1
Amine El Kouhen <amine.elkouhen@redis.com>
2022-10-11 15:40:50 +0200

    wrapping scripts into one main

--- main/GCP/GKE/Cross-Region/Basic_Clusters/config/create_crdb.sh
+++ main/GCP/GKE/Cross-Region/Basic_Clusters/config/create_crdb.sh
@@ -1,4 +1,11 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'Mandatory arguments not supplied !'
+    exit 1
+fi
+
 echo "=== Create a Conflict-free Replicated Database (CRDB)... ==="
+echo "Cluster 1: $1 - Cluster 2: $3"
+
 kubectl exec -it redis-cluster-0 -- \
 crdb-cli crdb create \
   --name mydb \
@@ -9,8 +16,8 @@ crdb-cli crdb create \
   --sharding true \
   --shards-count 2 \
   --encryption yes \
-  --instance fqdn=redis-cluster.NAMESPACE_CLUSTER_1.svc.cluster.local,url=https://api.IPADDRESS_CLUSTER_1,username=admin@admin.com,password=admin,replication_endpoint=mydb-db.IPADDRESS_CLUSTER_1:443,replication_tls_sni=mydb-db.IPADDRESS_CLUSTER_1 \
-  --instance fqdn=redis-cluster.NAMESPACE_CLUSTER_2.svc.cluster.local,url=https://api.IPADDRESS_CLUSTER_2,username=admin@admin.com,password=admin,replication_endpoint=mydb-db.IPADDRESS_CLUSTER_2:443,replication_tls_sni=mydb-db.IPADDRESS_CLUSTER_2
+  --instance fqdn=redis-cluster.$2.svc.cluster.local,url=https://api.$1,username=admin@admin.com,password=admin,replication_endpoint=mydb-db.$1:443,replication_tls_sni=mydb-db.$1 \
+  --instance fqdn=redis-cluster.$4.svc.cluster.local,url=https://api.$3,username=admin@admin.com,password=admin,replication_endpoint=mydb-db.$3:443,replication_tls_sni=mydb-db.$3
 
 echo "=== Create a Conflict-free Replicated Database (CRDB)... ==="
 echo "Now you can test your CRDB with the command:"
