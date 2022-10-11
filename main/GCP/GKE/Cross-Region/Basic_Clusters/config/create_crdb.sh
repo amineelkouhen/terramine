eaf8dd8dce10fba58290c06c2fe17a5ba90492df
Amine El Kouhen <amine.elkouhen@redis.com>
2022-10-11 11:49:17 +0200

    missing encryption property in the CRDB script

--- main/GCP/GKE/Cross-Region/Basic_Clusters/config/create_crdb.sh
+++ main/GCP/GKE/Cross-Region/Basic_Clusters/config/create_crdb.sh
@@ -1,16 +1,17 @@
 echo "=== Create a Conflict-free Replicated Database (CRDB)... ==="
 kubectl exec -it redis-cluster-0 -- \
 crdb-cli crdb create \
-  --name redis-db \
+  --name mydb \
   --port 18000 \
   --password testPassword \
   --memory-size 2GB \
   --replication true \
   --sharding true \
   --shards-count 2 \
-  --instance fqdn=redis-cluster.NAMESPACE_CLUSTER_1.svc.cluster.local,url=https://api.IPADDRESS_CLUSTER_1,username=admin@admin.com,password=admin,replication_endpoint=redis-db.IPADDRESS_CLUSTER_1:443,replication_tls_sni=redis-db.IPADDRESS_CLUSTER_1 \
-  --instance fqdn=redis-cluster.NAMESPACE_CLUSTER_2.svc.cluster.local,url=https://api.IPADDRESS_CLUSTER_2,username=admin@admin.com,password=admin,replication_endpoint=redis-db.IPADDRESS_CLUSTER_2:443,replication_tls_sni=redis-db.IPADDRESS_CLUSTER_2
+  --encryption yes \
+  --instance fqdn=redis-cluster.NAMESPACE_CLUSTER_1.svc.cluster.local,url=https://api.IPADDRESS_CLUSTER_1,username=admin@admin.com,password=admin,replication_endpoint=mydb-db.IPADDRESS_CLUSTER_1:443,replication_tls_sni=mydb-db.IPADDRESS_CLUSTER_1 \
+  --instance fqdn=redis-cluster.NAMESPACE_CLUSTER_2.svc.cluster.local,url=https://api.IPADDRESS_CLUSTER_2,username=admin@admin.com,password=admin,replication_endpoint=mydb-db.IPADDRESS_CLUSTER_2:443,replication_tls_sni=mydb-db.IPADDRESS_CLUSTER_2
 
 echo "=== Create a Conflict-free Replicated Database (CRDB)... ==="
 echo "Now you can test your CRDB with the command:"
-echo "kubectl exec -it redis-cluster-0 -- redis-cli -h redis-db -p 18000 -a testPassword"
+echo "kubectl exec -it redis-cluster-0 -- redis-cli -h mydb -p 18000 -a testPassword"
