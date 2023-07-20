d8dc6dcac176f5f4a712dc91b8beeccc617ebbe9
amineelkouhen <amine.elkouhen@redis.com>
2023-07-20 17:35:42 +0200

    Installing Docker + Adding Automatic provisionning of Grafana dashboards

--- modules/azure/bastion/bastion.tf
+++ modules/azure/bastion/bastion.tf
@@ -247,6 +247,76 @@ resource "azurerm_linux_virtual_machine" "client" {
     sudo systemctl status prometheus >> /home/${var.ssh_user}/prometheus_status.log 
     sudo systemctl status grafana-server >> /home/${var.ssh_user}/grafana_status.log 
 
+    ################
+    # Install Docker
+    echo "$(date) - Installing Docker" >> /home/${var.ssh_user}/prepare_client.log
+    sudo apt update >> /home/${var.ssh_user}/prepare_client.log 2>&1
+    sudo apt -y install apt-transport-https ca-certificates curl software-properties-common >> /home/${var.ssh_user}/prepare_client.log 2>&1
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >> /home/${var.ssh_user}/prepare_client.log 2>&1
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" >> /home/${var.ssh_user}/prepare_client.log 2>&1
+    sudo apt -y install docker-ce >> /home/${var.ssh_user}/prepare_client.log 2>&1
+    sudo groupadd docker
+    sudo usermod -aG docker ${var.ssh_user}
+
+    ################
+    # Link Grafana to Prometheus
+    echo "$(date) - Link Grafana to Prometheus" >> /home/${var.ssh_user}/prepare_client.log
+    echo "apiVersion: 1
+    datasources:
+    - name: Prometheus
+        type: prometheus
+        access: proxy
+        url: http://127.0.0.1:9090
+    " | sudo tee /etc/grafana/provisioning/datasources/prometheus.yaml
+
+    ################
+    # Dashboards provisionning
+    echo "$(date) - Dashboards provisionning" >> /home/${var.ssh_user}/prepare_client.log
+    echo "apiVersion: 1
+    providers:
+    - name: 'default'
+        orgId: 1
+        folder: ''
+        type: file
+        options:
+        path: /var/lib/grafana/dashboards
+    " | sudo tee /etc/grafana/provisioning/dashboards/dashboards.yaml
+
+    sudo mkdir -p /var/lib/grafana/dashboards
+    sudo chown -R grafana.grafana /var/lib/grafana/dashboards
+    sudo systemctl restart grafana-server
+
+    ################
+    # Adding Dashboard: Redis cluster dashboards (18405)
+    echo "$(date) - Adding Dashboard: redis cluster (18405)" >> /home/${var.ssh_user}/prepare_client.log
+    sudo wget -O /var/lib/grafana/dashboards/18405-cluster-status.json https://grafana.com/api/dashboards/18405/revisions/1/download
+    echo "Replacing $${DS_PROMETHEUS} with Prometheus" >> /home/${var.ssh_user}/prepare_client.log
+    sudo sed -i 's/\$${DS_PROMETHEUS}/Prometheus/' /var/lib/grafana/dashboards/18405-cluster-status.json
+
+    ################
+    # Adding Dashboard: Redis database dashboards (18408)
+    echo "$(date) - Adding Dashboard: Redis Database Dashboards (18408)" >> /home/${var.ssh_user}/prepare_client.log
+    sudo wget -O /var/lib/grafana/dashboards/18408-database-status-dashboard.json https://grafana.com/api/dashboards/18408/revisions/2/download
+    echo "Replacing $${DS_PROMETHEUS} with Prometheus" >> /home/${var.ssh_user}/prepare_client.log
+    sudo sed -i 's/\$${DS_PROMETHEUS}/Prometheus/' /var/lib/grafana/dashboards/18408-database-status-dashboard.json
+
+    ################
+    # Adding Dashboard: Redis nodes metrics dashboard
+    echo "$(date) - Adding Dashboard: Redis nodes metrics dashboard" >> /home/${var.ssh_user}/prepare_client.log
+    sudo wget -O /var/lib/grafana/dashboards/redis-software-node-dashboard.json https://raw.githubusercontent.com/redis-field-engineering/redis-enterprise-grafana-dashboards/main/dashboards/software/basic/redis-software-node-dashboard.json
+    echo "Replacing $${DS_PROMETHEUS} with Prometheus" >> /home/${var.ssh_user}/prepare_client.log
+    sudo sed -i 's/\$${DS_PROMETHEUS}/Prometheus/' /var/lib/grafana/dashboards/redis-software-node-dashboard.json
+
+    # Adding Dashboard: Redis shards metrics dashboard
+    echo "$(date) - Adding Dashboard: Redis shards metrics dashboard" >> /home/${var.ssh_user}/prepare_client.log
+    sudo wget -O /var/lib/grafana/dashboards/redis-software-shard-dashboard.json https://raw.githubusercontent.com/redis-field-engineering/redis-enterprise-grafana-dashboards/main/dashboards/software/basic/redis-software-shard-dashboard.json
+    echo "Replacing $${DS_PROMETHEUS} with Prometheus" >> /home/${var.ssh_user}/prepare_client.log
+    sudo sed -i 's/\$${DS_PROMETHEUS}/Prometheus/' /var/lib/grafana/dashboards/redis-software-shard-dashboard.json
+
+    # Restart Grafana
+    echo "$(date) - Restart Grafana Server" >> /home/${var.ssh_user}/prepare_client.log
+    sudo systemctl restart grafana-server
+
     echo "$(date) - DONE creating client" >> /home/${var.ssh_user}/prepare_client.log
     EOF
     )
